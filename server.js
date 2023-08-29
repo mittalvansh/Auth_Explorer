34fb64082332cf96b049bddcdaea0d31877f4577
Vansh Mittal <mittalvansh69@gmail.com>
2023-08-29 21:47:31 +0530

    Added cors!

--- server.js
+++ server.js
@@ -5,14 +5,31 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const errorHandler = require("./middleware/error");
 const passport = require("passport");
+var cors = require("cors");
 
 // Load env vars
 dotenv.config({ path: ".env" });
+
+var corsOptions = {
+  origin: process.env.CLIENT_APP_URL, // Define your client app url here
+  optionsSuccessStatus: 200,
+};
 
 const app = express();
 
 // Body parser
 app.use(express.json());
+
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+app.use(cors(corsOptions));
 
 // Cookie parser
 app.use(cookieParser());
